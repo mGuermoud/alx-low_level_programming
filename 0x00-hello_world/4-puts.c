1450e830eb7760b405be4900b83ab241622bd5e8
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-03-16 13:16:03 -0700

    tested by betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is a main function
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
