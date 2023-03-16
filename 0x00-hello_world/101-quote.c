11214d9787761279c6827cc9e3bec83910ea5ff5
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-03-16 14:13:13 -0700

    print an output to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,9 @@
 int main(void)
 {
 
-	fprintf(stdout, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stdout,
+	"%s",
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 
 }
