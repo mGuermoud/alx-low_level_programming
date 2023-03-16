14c922cbb598016207eb635aaa6c013809b98ff2
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-03-16 13:23:06 -0700

    print string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - print a simple string using printf
+ *
+ * return: void
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
