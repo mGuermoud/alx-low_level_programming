3e09b4fdc72ddcb7f74b13f4b14677da0934650b
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-03-16 14:08:40 -0700

    print an output to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print a string in the standard error
+ * Return: void
+ */
+
+int main(void)
+{
+
+	fprintf(stdout, "%s", "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	return (1);
+
+}
