65918dedba6c6038b122e12d71894e1871ec4c86
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-03-18 07:40:18 -0700

    print all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,51 @@
+/*
+ * File: 102-print_comb5.c
+ * Auth: Mohammed Guermoud
+ * Date: 17-04-2023
+ */
+
+#include <stdio.h>
+
+/**
+ * main - printing all alphabet in lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+	int k = '0';
+	int l = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = i; k <= '9'; k++)
+			{
+				for (l = j + 1; l <= '9'; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i == '9' && j == '8' && k == '9' && '9')
+					{
+						/* do nothing */
+					}
+					else
+					{
+						putchar(',');
+					}
+
+				}
+			}
+		}
+	}
+	putchar(0xA);
+
+	return (0);
+
+}
