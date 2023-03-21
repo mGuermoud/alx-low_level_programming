1c2e3c3829b91e726f4ff0383c2345be117a856b
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-03-21 14:25:49 -0700

    betty style correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * 1-alphabet - print alphabet in lower case
+ * print_alphabet - print alphabet in lower case
  * Return: always 0
  */
 
 void print_alphabet(void)
 {
 	/* declare local variable */
-	int a='a';
+	int a = 'a';
 
-	while(a <= 122 )
+	while (a <= 122)
 	{
 		_putchar(a);
 		a = a + 1;
