31c26f32545bacbbd037e6bfcdf57bb16b39a49a
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-03-23 15:48:06 -0700

    apply betty rules

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,38 +8,35 @@
  */
 int main(void)
 {
-    int i = 1;
+	int i = 1;
 
-    while (i <= 100)
-    {
-	if (i % 3 == 0 && i % 5 == 0)
+	while (i <= 100)
 	{
-		printf("FizzBuzz");
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		i++;
+		if (i <= 100)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
 	}
-	else if (i % 3 == 0)
-	{
-		printf("Fizz");
-	}
-	else if (i % 5 == 0)
-	{
-		printf("Buzz");
-	}
-	else
-	{
-		printf("%d",i);
-	}
-	i++;
-	if (i <= 100)
-	{
-		printf(" ");
-	}
-	else
-	{
-		printf("\n");
-	}
-
-
-    }
-
-    return (0);
+	return (0);
 }
