fc7afc58f204a9c4c82dfdcc868fd070d4a7c32f
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-03-23 15:45:00 -0700

    run fizzbuzz app

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the FizzBuzz function
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i = 1;
+
+    while (i <= 100)
+    {
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+		printf("FizzBuzz");
+	}
+	else if (i % 3 == 0)
+	{
+		printf("Fizz");
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else
+	{
+		printf("%d",i);
+	}
+	i++;
+	if (i <= 100)
+	{
+		printf(" ");
+	}
+	else
+	{
+		printf("\n");
+	}
+
+
+    }
+
+    return (0);
+}
