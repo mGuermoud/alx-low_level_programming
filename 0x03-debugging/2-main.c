5caadd0049315beb8d837b97fd844a492bfbf497
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-03-23 06:41:41 -0700

    correct the logic

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,12 +11,11 @@ int main(void)
 	int a, b, c;
 	int largest;
 
-	a = 972;
-	b = -98;
-	c = 0;
+	a = -972;
+	b = -988888;
+	c = -999999;
 
 	largest = largest_number(a, b, c);
-
 	printf("%d is the largest number\n", largest);
 
 	return (0);
