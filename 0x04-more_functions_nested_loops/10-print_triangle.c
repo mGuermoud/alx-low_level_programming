ac4ed1533e416b5720351689a0b57088b7610309
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-03-23 16:08:16 -0700

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_triangle - print triangle
+ * @size : number of lines to print
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i = 0;
+	int c = 0;
+	int k = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (k = 0; k < size - c; k++)
+			{
+				_putchar(' ');
+			}
+			c++;
+			for (k = 0; k < c; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
