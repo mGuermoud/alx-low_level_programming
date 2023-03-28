381bc67efa496908b9e7c4819e77bd799416fc87
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-03-28 05:29:16 -0700

    print nothing when n is null

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,16 +4,24 @@
 /**
  * print_array - print an array
  * @a: parameter a
- * @n: parameter n 
+ * @n: parameter n
  * Return: void
  */
 
 void print_array(int *a, int n)
 {
 	int i = 0;
-	for (i = 0; i < n - 1; i++)
+
+	if (n > 0)
 	{
-		printf("%d, ", *(a + i));
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d, ", *(a + i));
+		}
+		printf("%d\n", *(a + n - 1));
 	}
-	printf("%d\n", *(a + n - 1));
+	else
+	{
+		printf("\n");
+	}
 }
