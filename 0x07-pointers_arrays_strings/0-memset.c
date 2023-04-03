3a57f986008368fcd0e7ccc1dc12d67889581011
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-04-03 14:15:08 -0700

    correct betty rules

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,14 +12,14 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
-	
-	if (s == NULL) 
+
+	if (s == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < n; i++)
 	{
-		*(s+i) = b;
+		*(s + i) = b;
 	}
 	return (s);
 }
