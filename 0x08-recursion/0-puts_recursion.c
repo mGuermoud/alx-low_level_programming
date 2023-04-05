226687a2b772604c05c4da496f4fd18399655815
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-04-05 14:35:03 -0700

    print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string followed by a new line
+ * @s: 1st param
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion((s + 1));
+	}
+
+}
+
