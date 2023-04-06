e9378d9927a41cfa242b5fc24580987ae769b78b
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-04-06 14:48:32 -0700

    print args number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - print the arguments number
+* Return: 1
+*/
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n",argc - 1);
+	return (1);
+}
