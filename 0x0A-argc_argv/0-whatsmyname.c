df6eb4e9e55ed737e94c20674b7284cd9745f993
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-04-06 14:45:43 -0700

    print the program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - print the program name
+* Return: 1
+*/
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+	return (1);
+}
