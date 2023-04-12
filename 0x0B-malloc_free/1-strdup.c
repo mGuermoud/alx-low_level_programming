a4f7f21b952ee09f82716ce3878d8531b3b5c1c9
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-04-12 16:47:44 -0700

    strdup implementation

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strdup - duplicate a memory zone
+ * @str: array to be duplicated
+ *
+ * Return: a pointer to the created memory zone
+ * On error, NULL is returned.
+ */
+char *_strdup(char *str)
+{
+	char *copy = NULL;
+	unsigned int l = 0;
+	unsigned int i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (*(str + l) != '\0')
+	{
+		l++;
+	}
+	copy = (char *) malloc(l * sizeof(char));
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (i < l)
+		{
+			*(copy + i) = *(str + i);
+			i++;
+		}
+	}
+	return (copy);
+}
