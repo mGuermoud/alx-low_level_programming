270e764d187daa07dacf88fc84fc4b9710f437ec
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-04-11 17:55:19 -0700

    change the change program logic

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,28 +10,29 @@
 
 int main(int argc, char *argv[])
 {
-	int cents = 0;
-	int coins = cents / 25;
+	int coins[] = {25, 10, 5, 2, 1};
+	int i = 0, j = 0;
+	int result = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
-	if (cents < 0)
+	j = atoi(argv[1]);
+	if (j < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	cents %= 25;
-	coins += cents / 10;
-	cents %= 10;
-	coins += cents / 5;
-	cents %= 5;
-	coins += cents / 2;
-	cents %= 2;
-	coins += cents;
-	printf("%d\n", coins);
+	for (i = 0; i < 4 && j >= 0; i++)
+	{
+		while (j >= coins[i])
+		{
+			result = result + 1;
+			j = j - coins[i];
+		}
+	}
+	printf("%d\n", result);
 	return (0);
 }
