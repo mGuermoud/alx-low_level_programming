35a410b95ff22fec21d05ba076956a8f37ee3b09
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-04-13 17:02:45 -0700

    correct the output(

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int l1 = 0, l2 = 0, i = 0, j = 0;
 	char *copy;
-
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (*(s1 + l1) != '\0')
 		l1++;
 	while (*(s2 + l2) != '\0')
