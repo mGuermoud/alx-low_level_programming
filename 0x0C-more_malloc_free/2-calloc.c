4aae52c734ebfa26c3af80d0bb453aa319ef8c03
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-04-13 17:18:18 -0700

    implement calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocate mem for an array
+ * @nmemb: first parameter
+ * @size: array size
+ *
+ * Return: pointer to the new memory zone.
+ * On error, return NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i = 0;
+
+	if (!nmemb || !size)
+		return (NULL);
+	ptr = (char *) malloc(nmemb * size * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
+
+}
