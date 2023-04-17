ad09606b877e07e0b78b7ac59bb070c0536ce780
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-04-17 13:27:25 -0700

    define a struct called dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog definition
+ * @name: dog name
+ * @age:  dog age
+ * @owner: dog owner
+ *
+ * Description: define a dog struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
