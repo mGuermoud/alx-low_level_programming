ab7f0c7a01c2056971871c96cfe834f4ef6aed17
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-04-17 14:07:31 -0700

    print struct of type dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 		{
 			printf("Name : (nil)\n");
 		}
-		printf("Age: %.6f\n", d->age);
+		printf("Age: %f\n", d->age);
 		if (d->owner != NULL)
 		{
 			printf("Owner: %s\n", d->owner);
