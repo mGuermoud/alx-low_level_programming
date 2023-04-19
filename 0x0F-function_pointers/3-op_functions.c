d4d6e1922ff0234a150569912880d6971d0340e1
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-04-19 16:59:45 -0700

    initial version of the program

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - return the sum of two int
+ * @a: first param
+ * @b: second param
+ *
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - return the difference of two int
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - return the product of two int
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - retunr the division of two int
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - return the remainder of two int
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
