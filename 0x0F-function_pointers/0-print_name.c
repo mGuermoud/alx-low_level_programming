836b790e898d4a247fc11d0eab6f88c432e32674
Mohammed GUERMOUD <mohammed.guermoud@gmail.com>
2023-04-19 13:57:48 -0700

    print a name passed by address

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name passed by address
+ * @name: name of the person
+ * @f: the function that print a name as is or in uppercase
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	((*f)(name));
+}
